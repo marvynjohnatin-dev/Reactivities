28ae13271ddaf7a837542567f396691b1b6ed42f
marvynjohnatin <marvynjohnatin.dev@gmail.com>
2025-03-11 16:31:21 +0800

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -16,6 +17,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddCors();
 builder.Services.AddMediatR(x => 
     x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
